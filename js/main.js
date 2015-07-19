e703c208d958f2959fa9b5def337ae47d289e875
yashshah12 <ybshah12@gmail.com>
2015-07-18 22:42:51 -0400

    adding project modals

--- js/main.js
+++ js/main.js
@@ -32,21 +32,27 @@
         }, {
             offset: '50%'
         });
-        // Typing Animation
-        $("#typed").typed({
-            strings: ["A Developer", "A Designer", "A Student"],
-            typeSpeed: 30,
-            backDelay: 500,
-            loop: false,
-            contentType: 'html', // or text
-            // defaults to false for infinite loop
-            loopCount: false,
-            callback: function(){ foo(); },
-            resetCallback: function() { newTyped(); }
-        });
+        // // Typing Animation
+        // $("#typed").typed({
+        //     strings: ["A Developer", "A Designer", "A Student"],
+        //     typeSpeed: 30,
+        //     backDelay: 500,
+        //     loop: false,
+        //     contentType: 'html', // or text
+        //     // defaults to false for infinite loop
+        //     loopCount: false,
+        //     callback: function(){ foo(); },
+        //     resetCallback: function() { newTyped(); }
+        // });
 
-        $(".reset").click(function(){
-            $("#typed").typed('reset');
+        // $(".reset").click(function(){
+        //     $("#typed").typed('reset');
+        // });
+        $( ".cta" ).click(function() {
+          $('#projects').hide();
+        });
+        $(".close-modal").click(function() {
+          $('#projects').show();
         });
     });
 
