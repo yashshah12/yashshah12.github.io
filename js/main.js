d12547330ee5275908ec86c01a52875f0348ac0a
yashshah12 <ybshah12@gmail.com>
2015-09-09 00:30:51 -0400

    redesigning and adding new resume

--- js/main.js
+++ js/main.js
@@ -1,57 +1,13 @@
 (function ($) {
     $(function () {
         var viewport = $('html, body');
-        $('#home').waypoint(function () {
-            setTimeout(function () {
-                $('#home-header').addClass('animated bounceIn')
-            }, 0);
-            setTimeout(function () {
-                $('#home-image').addClass('animated fadeInDown')
-            }, 500);
-        }, {
-            offset: '50%'
-        });
-        // // Typing Animation
-        // $("#typed").typed({
-        //     strings: ["A Developer", "A Designer", "A Student"],
-        //     typeSpeed: 30,
-        //     backDelay: 500,
-        //     loop: false,
-        //     contentType: 'html', // or text
-        //     // defaults to false for infinite loop
-        //     loopCount: false,
-        //     callback: function(){ foo(); },
-        //     resetCallback: function() { newTyped(); }
-        // });
-
-        // $(".reset").click(function(){
-        //     $("#typed").typed('reset');
-        // });
-        $( ".cta" ).click(function() {
+        $( ".proj" ).click(function() {
           $('#projects').hide();
         });
         $(".close-modal").click(function() {
           $('#projects').show();
         });
 
-        $('#game-link').click(function (event) {
-            event.preventDefault();
-            viewport.bind("scroll mousedown DOMMouseScroll mousewheel keyup", function () {
-                viewport.stop();
-            });
-            viewport.animate({
-                scrollTop: $('#projects').offset().top
-            }, 700);
-        });
-        $('#work-link').click(function (event) {
-            event.preventDefault();
-            viewport.bind("scroll mousedown DOMMouseScroll mousewheel keyup", function () {
-                viewport.stop();
-            });
-            viewport.animate({
-                scrollTop: $('#work').offset().top
-            }, 700);
-        });
     });
 
 })(jQuery);
